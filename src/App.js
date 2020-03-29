51730e6b7f35ef6e700147cf9c2d73b48bba8370
Ifunanyachi Kanu <ifunanyachi.kanu@outlook.com>
2020-03-29 22:11:30 +0100

    app

--- src/App.js
+++ src/App.js
@@ -43,4 +43,57 @@ function PublicRoute({ component: Component, authenticated, ...rest }) {
   );
 }
 
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      authenticated: false,
+      loading: true
+    };
+  }
+
+  componentDidMount() {
+    auth().onAuthStateChanged(user => {
+      if (user) {
+        this.setState({
+          authenticated: true,
+          loading: false
+        });
+      } else {
+        this.setState({
+          authenticated: false,
+          loading: false
+        });
+      }
+    });
+  }
+
+  render() {
+    return this.state.loading === true ? (
+      <h2>Loading...</h2>
+    ) : (
+      <Router>
+        <Switch>
+          <Route exact path="/" component={Home}></Route>
+          <PrivateRoute
+            path="/chat"
+            authenticated={this.state.authenticated}
+            component={Chat}
+          ></PrivateRoute>
+          <PublicRoute
+            path="/signup"
+            authenticated={this.state.authenticated}
+            component={Signup}
+          ></PublicRoute>
+          <PublicRoute
+            path="/login"
+            authenticated={this.state.authenticated}
+            component={Login}
+          ></PublicRoute>
+        </Switch>
+      </Router>
+    );
+  }
+}
+
 export default App;
