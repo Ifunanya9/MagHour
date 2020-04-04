480ceed8dfe2475d4994f697ef7babf560a7771c
derfea <42876528+chikamso@users.noreply.github.com>
2020-04-04 22:55:38 +0100

    start checks

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import React, { Component } from "react";
 import {
-  Route,
-  BrowserRouter as Router,
-  Switch,
-  Redirect
+    Route,
+    BrowserRouter as Router,
+    Switch,
+    Redirect
 } from "react-router-dom";
 import Home from "./pages/Home";
 import Chat from "./pages/Chat";
@@ -13,94 +13,100 @@ import About from "./pages/About";
 import { auth } from "./services/firebase";
 import "./styles.css";
 
-function PrivateRoute({ component: Component, authenticated, ...rest }) {
-  return (
-    <Route
-      {...rest}
-      render={props =>
-        authenticated === true ? (
-          <Component {...props} />
-        ) : (
-          <Redirect
-            to={{ pathname: "/login", state: { from: props.location } }}
-          />
-        )
-      }
-    />
-  );
+const PrivateRoute = ({ component: Component, authenticated, ...rest }) => {
+    console.log(Component);
+    return ( < Route {...rest }
+        render = {
+            props =>
+            authenticated === true ? ( < Component {...props }
+                />
+            ) : ( <
+                Redirect to = {
+                    { pathname: "/login", state: { from: props.location } }
+                }
+                />
+            )
+        }
+        />
+    );
 }
 
-function PublicRoute({ component: Component, authenticated, ...rest }) {
-  console.log(Component);
-  return (
-    <Route
-      {...rest}
-      render={props =>
-        authenticated === false ? (
-          <Component {...props} />
-        ) : (
-          <Redirect to="/chat" />
-        )
-      }
-    />
-  );
+const PublicRoute = ({ component: Component, authenticated, ...rest }) => {
+    console.log(Component);
+    return ( <
+        Route {...rest }
+        render = {
+            props =>
+            authenticated === false ? ( <
+                Component {...props }
+                />
+            ) : ( <
+                Redirect to = "/chat" / >
+            )
+        }
+        />
+    );
 }
 
 class App extends Component {
-  constructor() {
-    super();
-    this.state = {
-      authenticated: false,
-      loading: true
-    };
-  }
+    constructor() {
+        super();
+        this.state = {
+            authenticated: false,
+            loading: true
+        };
+    }
 
-  componentDidMount() {
-    auth().onAuthStateChanged(user => {
-      if (user) {
-        this.setState({
-          authenticated: true,
-          loading: false
+    componentDidMount() {
+        auth().onAuthStateChanged(user => {
+            if (user) {
+                this.setState({
+                    authenticated: true,
+                    loading: false
+                });
+            } else {
+                this.setState({
+                    authenticated: false,
+                    loading: false
+                });
+            }
         });
-      } else {
-        this.setState({
-          authenticated: false,
-          loading: false
-        });
-      }
-    });
-  }
+    }
 
-  render() {
-    return this.state.loading === true ? (
-      <div className="spinner-border text-success" role="status">
-        <span className="sr-only">Loading...</span>
-      </div>
-    ) : (
-      <Router>
-        <Switch>
-          <Route exact path="/" component={Home} />
+    render() {
+        return this.state.loading === true ? ( <
+            div className = "spinner-border text-success"
+            role = "status" >
+            <
+            span className = "sr-only" > Loading... < /span>  <
+            /div >
+        ) : ( < Router >
+            <
+            Switch >
+            <
+            Route exact path = "/"
+            component = { Home }
+            />
 
-          <PrivateRoute
-            path="/chat"
-            authenticated={this.state.authenticated}
-            component={Chat}
-          />
-          <PublicRoute
-            path="/signup"
-            authenticated={this.state.authenticated}
-            component={Signup}
-          />
-          <PublicRoute
-            path="/login"
-            authenticated={this.state.authenticated}
-            component={Login}
-          />
-          <Route path="/about" component={About} />
-        </Switch>
-      </Router>
-    );
-  }
+            <
+            PrivateRoute path = "/chat"
+            authenticated = { this.state.authenticated }
+            component = { Chat }
+            /> <
+            PublicRoute path = "/signup"
+            authenticated = { this.state.authenticated }
+            component = { Signup }
+            /> <
+            PublicRoute path = "/login"
+            authenticated = { this.state.authenticated }
+            component = { Login }
+            /> <
+            Route path = "/about"
+            component = { About }
+            /> </
+            Switch > < /Router>
+        );
+    }
 }
 
 export default App;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -5,41 +5,52 @@ import { Link } from "react-router-dom";
 import "./home.css";
 
 export default class HomePage extends Component {
-  render() {
-    return (
-      <div className="home">
-        <Header></Header>
-        <section>
-          <div className="footer">
-            <div className="jumbotron jumbotron-fluid py-5">
-              <div className="container text-center py-5">
-                <h1 className="display-4 welcome">Welcome to MagHour</h1>
-                <br />
-                <p className="lead">
-                  A great place to share your thoughts with friends
-                </p>
-                <br />
-                <div className="mt-4">
-                  <Link
-                    className="btn btn-primary px-5 mr-3 create"
-                    to="/signup"
-                  >
-                    Create New Account
-                  </Link>
+    render() {
+        return ( <
+            div className = "home" >
+            <
+            Header / >
+            <
+            section >
+            <
+            div className = "footer" >
+            <
+            div className = "jumbotron jumbotron-fluid py-5" >
+            <
+            div className = "container text-center py-5" >
+            <
+            h1 className = "display-4 welcome" > Welcome to MagHour < /h1> <
+            br / >
+            <
+            p className = "lead" >
+            A great place to share your thoughts with friends <
+            /p> <
+            br / >
+            <
+            div className = "mt-4" >
+            <
+            Link className = "btn btn-primary px-5 mr-3 create"
+            to = "/signup" >
+            Create New Account <
+            /Link>
 
-                  <Link className="btn px-5 log" to="/login">
-                    Login to Your Account
-                  </Link>
-                </div>
-              </div>
-            </div>
-            <br />
-            <div className="foot">
-              <Footer></Footer>
-            </div>
-          </div>
-        </section>
-      </div>
-    );
-  }
+            <
+            Link className = "btn px-5 log"
+            to = "/login" >
+            Login to Your Account <
+            /Link> <
+            /div> <
+            /div> <
+            /div> <
+            br / >
+            <
+            div className = "foot" >
+            <
+            Footer > < /Footer> <
+            /div> <
+            /div> <
+            /section> <
+            /div>
+        );
+    }
 }
