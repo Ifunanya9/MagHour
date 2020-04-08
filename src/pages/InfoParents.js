f14ebd1258219db46af0e8ad10578e9a7817c572
derfea <42876528+chikamso@users.noreply.github.com>
2020-04-08 16:23:42 +0100

    cool

--- src/pages/InfoParents.js
+++ src/pages/InfoParents.js
@@ -8,6 +8,8 @@ export default class InfoParents extends Component {
             <Header/>
             <br/><br/><br/>
             <h1>Information for Parents</h1>
+            <p>Also if they're saying bad words or mean words we will send a email to you.<br/>After the school creates an account for your child you can access anything on your childs account like you can log in to your account.<br/>You can also veiw what your children are chatting about because if they're chat about something like lets meet out on Saturday and you don't know about it your child might be in danger.<br/>
+            </p>
         </div>
     )
 }
--- src/pages/InfoSchool.js
+++ src/pages/InfoSchool.js
@@ -8,6 +8,7 @@ export default class InfoSchool extends Component {
             <br/><br/><br/>
             <Header/>
             <h1>Information for Schools</h1>
+            <p>We create a account for the school.<br/>After the school gets the account then they create a account for the childrens with their parents account<br/>Also the school doesn't have to worry because this website is secure.<br/>Also the teachers can also chat with each other so it's not only the childrens that can chat to each other.</p>
         </div>
     )
 }
