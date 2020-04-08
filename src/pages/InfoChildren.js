fc60ca4c43822d5bbda4e9c221cd34886b2f8782
Ifunanyachi Kanu <ifunanyachi.kanu@outlook.com>
2020-04-08 17:16:38 +0100

    style

--- src/pages/InfoChildren.js
+++ src/pages/InfoChildren.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import Header from "../components/Header";
-import smile from "./boy.jpg";
+import smile from "../images.png";
 import "./home.css";
 
 export default class SignUp extends Component {
