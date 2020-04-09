24ac1bca8dc72586d3e1f780fe40c81fd0381a1f
Ifunanyachi Kanu <ifunanyachi.kanu@outlook.com>
2020-04-09 23:11:11 +0100

    nice

--- src/pages/ChatClass.js
+++ src/pages/ChatClass.js
@@ -2,10 +2,8 @@ import React, { Component } from "react";
 import Header from "../components/Header";
 import { auth } from "../services/firebase";
 import { db } from "../services/firebase";
-import {Card} from "react-bootstrap"
+import { Card } from "react-bootstrap";
 import "./chat.css";
-
-
 
 export default class Chat extends Component {
   constructor(props) {
@@ -17,7 +15,7 @@ export default class Chat extends Component {
       readError: null,
       // uid: null,
       writeError: null,
-      loadingChats: false
+      loadingChats: false,
     };
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
@@ -29,12 +27,12 @@ export default class Chat extends Component {
     this.setState({ readError: null, loadingChats: true });
     const chatArea = this.myRef.current;
     try {
-      db.ref("chats").on("value", snapshot => {
+      db.ref("chats").on("value", (snapshot) => {
         let chats = [];
-        snapshot.forEach(snap => {
+        snapshot.forEach((snap) => {
           chats.push(snap.val());
         });
-        chats.sort(function(a, b) {
+        chats.sort(function (a, b) {
           return a.timestamp - b.timestamp;
         });
         this.setState({ chats });
@@ -48,7 +46,7 @@ export default class Chat extends Component {
 
   handleChange(event) {
     this.setState({
-      content: event.target.value
+      content: event.target.value,
     });
   }
 
@@ -61,7 +59,7 @@ export default class Chat extends Component {
         content: this.state.content,
         timestamp: Date.now(),
         uid: this.state.user.uid,
-        chatBy: this.state.user.email
+        chatBy: this.state.user.email,
       });
       this.setState({ content: "" });
       chatArea.scrollBy(0, chatArea.scrollHeight);
@@ -77,8 +75,9 @@ export default class Chat extends Component {
   formatTime(timestamp) {
     const d = new Date(timestamp);
     const time = `
-    ${d.getDate()}/${d.getMonth() +
-      1}/${d.getFullYear()} ${d.getHours()}:${d.getMinutes()} `;
+    ${d.getDate()}/${
+      d.getMonth() + 1
+    }/${d.getFullYear()} ${d.getHours()}:${d.getMinutes()} `;
     return time;
   }
 
@@ -88,65 +87,68 @@ export default class Chat extends Component {
         <Header />
         <br />
         <div className="container">
-        <div className="chat-area" ref={this.myRef}>
-          {this.state.loadingChats ? (
-            <div className="spinner-border text-success" role="status">
-              <span className="sr-only">Loading...</span>
-            </div>
-          ) : (
-            ""
-          )}
-          {this.state.chats.map(chat => {
-            console.log(chat.chatBy)
-            return (
-              <Card className={"chat-bubble text-center " + (this.state.user.uid === chat.uid ? "current-user" : "")}>
-              <Card.Header >
-                <strong>{chat.chatBy}</strong>
-              <br/>
-                <strong>{this.formatTime(chat.timestamp)}</strong>
-              </Card.Header>
-              <Card.Body>
-                <Card.Text>
-                  {chat.content}
-                </Card.Text>
-                {/* <Card.Footer>
+          <div className="chat-area" ref={this.myRef}>
+            {this.state.loadingChats ? (
+              <div className="spinner-border text-success" role="status">
+                <span className="sr-only">Loading...</span>
+              </div>
+            ) : (
+              ""
+            )}
+            {this.state.chats.map((chat) => {
+              console.log(chat.chatBy);
+              return (
+                <Card
+                  className={
+                    "chat-bubble text-center " +
+                    (this.state.user.uid === chat.uid ? "current-user" : "")
+                  }
+                >
+                  <Card.Header>
+                    <strong>{chat.chatBy}</strong>
+                    <br />
+                    <strong>{this.formatTime(chat.timestamp)}</strong>
+                  </Card.Header>
+                  <Card.Body>
+                    <Card.Text>{chat.content}</Card.Text>
+                    {/* <Card.Footer>
                   <button onclick="confirmation()">Delete</button>
                 </Card.Footer> */}
-              </Card.Body>
-            </Card>
-            )
-          })}
-        </div>
-        <div className="text">
-          <label htmlFor="text">Chat Here!</label>
-          <form onSubmit={this.handleSubmit} className="mx-3">
-            <textarea
-              className="form-control chat-bubble chat-form"
-              name="content"
-              onChange={this.handleChange}
-              value={this.state.content}
-              id="text"
-            ></textarea>
+                  </Card.Body>
+                </Card>
+              );
+            })}
+          </div>
+          <div className="text">
+            <label htmlFor="text">Chat Here!</label>
+            <form onSubmit={this.handleSubmit} className="mx-3">
+              <textarea
+                className="form-control chat-bubble chat-form"
+                name="content"
+                onChange={this.handleChange}
+                value={this.state.content}
+                id="text"
+              ></textarea>
 
-            {this.state.error ? (
-              <p className="text-danger">{this.state.error}</p>
-            ) : null}
-            {this.state.content ? (
-              this.state.content !== " " ? (
-                <button
-                  type="submit"
-                  className="btn btn-submit px-5 mt-4 button chat-bubble"
-                >
-                  Send
-                </button>
-              ) : null
-            ) : null}
-          </form>
-        </div>
-        <div className="py-5 mx-3 text">
-          Login in as:{" "}
-          <strong className="text-info">{this.state.user.email}</strong>
-        </div>
+              {this.state.error ? (
+                <p className="text-danger">{this.state.error}</p>
+              ) : null}
+              {this.state.content ? (
+                this.state.content !== " " ? (
+                  <button
+                    type="submit"
+                    className="btn btn-submit px-5 mt-4 button chat-bubble"
+                  >
+                    Send
+                  </button>
+                ) : null
+              ) : null}
+            </form>
+          </div>
+          <div className="py-5 mx-3 text">
+            Login in as:{" "}
+            <strong className="text-info">{this.state.user.email}</strong>
+          </div>
         </div>
       </div>
     );
--- src/pages/InfoSchool.js
+++ src/pages/InfoSchool.js
@@ -11,19 +11,26 @@ export default class InfoSchool extends Component {
         <br />
         <br />
         <br />
-        <img className="smile" src={smile} alt="smiling" />
-        <h1>Information for Schools</h1>
-        <p>
-          We create a account for the school.
-          <br />
-          After the school gets the account then they create a account for the
-          childrens with their parents email.
-          <br />
-          Also the school doesn't have to worry because this website is secure.
-          <br />
-          Also the teachers can also chat with each other so it's not only the
-          childrens that can chat to each other.
-        </p>
+        <section>
+          <div className="jumbotron-fluid py-5">
+            <div className="container text-center py-5">
+              <img className="smile" src={smile} alt="smiling" />
+              <h1>Information for Schools</h1>
+              <p>
+                We create a account for the school.
+                <br />
+                After the school gets the account then they create a account for
+                the childrens with their parents email.
+                <br />
+                Also the school doesn't have to worry because this website is
+                secure.
+                <br />
+                Also the teachers can also chat with each other so it's not only
+                the childrens that can chat to each other.
+              </p>
+            </div>
+          </div>
+        </section>
       </div>
     );
   }
